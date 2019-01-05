4df82f67db9166b5ccdb05569ef1631408b5694e
Maggie <mzhao3@stuy.edu>
2019-01-05 00:15:12 -0500

    layout of board

--- game.c
+++ game.c
@@ -1,5 +1,24 @@
 # include "game.h"
 
+
+
+/**
+  h   g   f   e   d   c   b   a
+1 ___ x01 ___ x02 ___ x03 ___ x04
+2 x05 ___ x06 ___ x07 ___ x08 ___
+3 ___ x09 ___ x10 ___ x11 ___ x12
+4 ___ ___ ___ ___ ___ ___ ___ ___
+5 ___ ___ ___ ___ ___ ___ ___ ___
+6 o01 ___ o02 ___ o03 ___ o04 ___
+7 ___ o05 ___ o06 ___ o07 ___ o08
+8 o09 ___ o10 ___ o11 ___ o12 ___
+
+x is white
+o is red
+X/O is kinged
+
+50 moves without a capture/kinging is a draw
+**/
 void display(char * board) {
 
 }
